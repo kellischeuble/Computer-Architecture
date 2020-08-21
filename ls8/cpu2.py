19b25b936e882a4e8aa3fede1ec19e3cf6dbabe6
Kelli Scheuble <github@scheuble.dev>
2020-08-21 09:03:10 -0700

    sprint MVP + 1 stretch

--- ls8/cpu2.py
+++ ls8/cpu2.py
@@ -7,7 +7,7 @@ PRN = 0b01000111
 HLT = 0b00000001
 MUL = 0b10100010
 ADD = 0b10100000
-PSH = 0b01000101
+PUSH = 0b01000101
 POP = 0b01000110
 CALL = 0b01010000
 RET = 0b00010001
@@ -30,8 +30,6 @@ class CPU:
         self.pc = 0
         self.running = True
         self.flag = None
-        self.stack_pointer = 0xf4
-        self.reg[7] = self.stack_pointer
         
         self.branchtable = {
             LDI: self.handleLDI,
@@ -50,6 +48,8 @@ class CPU:
 
         }
 
+        self.stack_pointer = 0xf4
+        self.reg[7] = self.stack_pointer
     def load(self):
         """Load a program into memory."""
         address = 0
@@ -65,18 +65,16 @@ class CPU:
         else:
             print('missing command line argument')
             sys.exit(0)
-            
     def ram_read(self, MAR):
         return self.ram[MAR]
-
     def ram_write(self, MAR, MDR):
         self.ram[MAR] = MDR
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+        elif op == "MUL":
+            self.reg[reg_a] = self.reg[reg_a] * self.reg[reg_b]
         elif op == "CMP":
             a = self.reg[reg_a]
             b = self.reg[reg_b]
@@ -86,16 +84,27 @@ class CPU:
                 self.flag = LFLAG
             else:
                 self.flag = EFLAG
-
+        elif op == 'AND':
+            self.reg[reg_a] = self.reg[reg_a] & self.reg[reg_b]
+        elif op == 'OR':
+            self.reg[reg_a] = self.reg[reg_a] | self.reg[reg_b]
+        elif op == 'XOR':
+            self.reg[reg_a] = self.reg[reg_a] ^ self.reg[reg_b]
+        elif op == 'NOT':
+            self.reg[reg_a] = ~self.reg[reg_a]
+        elif op == 'SHL':
+            self.reg[reg_a] = self.reg[reg_a] << self.reg[reg_b]
+        elif op == 'SHR':
+            self.reg[reg_a] = self.reg[reg_a] >> self.reg[reg_b]
+        elif op == 'MOD':
+            self.reg[reg_a] = self.reg[reg_a] % self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
-
     def trace(self):
         """
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
-
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
             # self.fl,
@@ -104,91 +113,80 @@ class CPU:
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
         ), end='')
-
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
-
         print()
-
-    def handleHLT(self, a, b):
+    def handleHLT(self, a=None, b=None):
         self.running = False
-
     def handleLDI(self, a, b):
         self.reg[a] = b
         self.pc += 3
-
-    def handlePRN(self, a, b):
+    def handlePRN(self, a, b=None):
         print(self.reg[a])
         self.pc += 2
-
     def handleMUL(self, a, b):
         self.reg[a] = self.reg[a] * self.reg[b]
         self.pc += 3
-
-    def handlePUSH(self, a, b):
-        self.sp -= 1
-        self.sp &= 0xff 
-
+    def handlePUSH(self, a, b=None):
+        # decrement stack pointer
+        self.stack_pointer -= 1
+        self.stack_pointer &= 0xff  # keep in range of 00-FF
+        # get register number and value stored at specified reg number
         reg_num = self.ram[self.pc + 1]
         val = self.reg[reg_num]
-
-        self.ram[self.sp] = val
+        # store value in ram
+        self.ram[self.stack_pointer] = val
         self.pc += 2
-
-    def handlePOP(self, a, b):
-        val = self.ram[self.sp]
+    def handlePOP(self, a, b=None):
+        # get value from RAM
+        val = self.ram[self.stack_pointer]
+        # store at given register
         reg_num = self.ram[self.pc + 1]
         self.reg[reg_num] = val
-        self.sp += 1
-        self.sp &= 0xff  # keep in range of 00-FF
-
+        # increment stack pointer and program counter
+        self.stack_pointer += 1
+        self.stack_pointer &= 0xff  # keep in range of 00-FF
         self.pc += 2
-
     def handleCALL(self, a, b):
+        # return counter, save to stack
         rc = self.pc + 2
-        self.sp -= 1
-        self.ram[self.sp] = rc
-
+        self.stack_pointer -= 1
+        self.ram[self.stack_pointer] = rc
         self.pc = self.reg[a]
-
     def handleRET(self, a, b):
-        val = self.ram[self.sp]
+        # pop from stack
+        val = self.ram[self.stack_pointer]
+        # set pc back to previous
         self.pc = val
-        self.sp += 1
-
+        # increment stack pointer
+        self.stack_pointer += 1
     def handleADD(self, a, b):
         self.alu('ADD', a, b)
         self.pc += 3
-
     def handleCMP(self, a, b):
         self.alu('CMP', a, b)
         self.pc += 3
-
     def handleJEQ(self, a, b):
         if self.flag == EFLAG:
             self.pc = self.reg[a]
         else:
             self.pc += 2
-
     def handleJNE(self, a, b):
         if self.flag != EFLAG:
             self.pc = self.reg[a]
         else:
             self.pc += 2
-
     def handleJMP(self, a, b):
         self.pc = self.reg[a]
-
     def run(self):
         """Run the CPU."""
-
         while self.running:
             IR = self.ram_read(self.pc)
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
             if IR not in self.branchtable:
                 print(f'unknown instruction {IR} at address {self.pc}')
-                self.running=False
+                self.running = False
             else:
                 f = self.branchtable[IR]
                 f(operand_a, operand_b)
